169b46ea2e2735f1c83b5893f1312e2cc8f68e74
hodonnell <hodonnell@nymbus.com>
2024-09-18 22:20:44 -0400

    color2

--- app/components/Project.js
+++ app/components/Project.js
@@ -29,11 +29,11 @@ export default function Project({ details, onMouseEnter, onMouseLeave, index}) {
             <div className="p-5 align-bottom">
                 <a href="#">
                     <h5 
-                        className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        className="mb-2 text-2xl font-bold tracking-tight text-white">
                         {details.title} | {details.date}
                     </h5>
                 </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="mb-3 font-normal text-gray-300">
                     {details.description}
                 </p>
             </div>
